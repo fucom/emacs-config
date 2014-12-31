d91e856e77a1f4688d12ec2d93e6d320b2522aed
Hamid Odabai <ofhamid@gmail.com>
2014-12-31 02:14:42 +0100

    Second commit

--- init.el
+++ init.el
@@ -46,6 +46,20 @@
 (put 'erase-buffer 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((reftex-default-bibliography "egbib.bib") (TeX-master . "../latex2014") (modee . latex)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; Local Variables:
 ;; mode: lisp
 ;; End:
