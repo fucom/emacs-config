e7116df13a1a27ce19805263679183b94f70bb2a
Hamid Odabai <ofhamid@gmail.com>
2015-02-09 09:19:25 +0100

    Ido like file browsing.

--- lisp/odabai-helm.el
+++ lisp/odabai-helm.el
@@ -23,7 +23,7 @@
   (define-key helm-find-files-map (kbd "<backspace>") 'helm-find-files-up-one-level)
   (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
   (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-  (dolist (ext '("\\.mtc*" "\\.blg$" "\\.bbl$"  "\\.bash_history"))
+  (dolist (ext '("\\.\\.$" "\\.bash_history"))
     (add-to-list 'helm-boring-file-regexp-list ext))
 
   ;; I don't like to see recent files in buffer list
@@ -33,6 +33,27 @@
     "Default sources list used in `helm-mini'."
     :group 'helm-misc
     :type '(repeat (choice symbol)))
+
+  ;; let <return> open directory or file
+  ;; http://emacs.stackexchange.com/questions/3798/how-do-i-make-pressing-ret-in-helm-find-files-open-the-directory
+  (defun odabai/helm-find-files-navigate-forward (orig-fun &rest args)
+    (if (file-directory-p (helm-get-selection))
+        (apply orig-fun args)
+      (helm-maybe-exit-minibuffer)))
+  (advice-add 'helm-execute-persistent-action :around #'odabai/helm-find-files-navigate-forward)
+  (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
+
+  ;; do not delete file path so strongly when I hit backspace
+  ;; http://emacs.stackexchange.com/questions/3798/how-do-i-make-pressing-ret-in-helm-find-files-open-the-directory
+  (defun odabai/helm-find-files-navigate-back (orig-fun &rest args)
+   (if (= (length helm-pattern) (length (helm-find-files-initial-input)))
+        (helm-find-files-up-one-level 1)
+      (apply orig-fun args)))
+  (advice-add 'helm-ff-delete-char-backward :around #'odabai/helm-find-files-navigate-back)
+
+  (define-key helm-find-files-map (kbd "<backspace>") 'helm-ff-delete-char-backward)
+  (lookup-key helm-find-files-map
+              (read-kbd-macro "<backspace>"))
 
   ;; (setq helm-idle-delay 0.1)
   ;; (setq helm-input-idle-delay 0.1)
