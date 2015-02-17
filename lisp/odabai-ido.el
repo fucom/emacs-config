e36cfb72db552ad84cac3fff707a3184b23f9a20
Hamid Odabai <ofhamid@gmail.com>
2015-02-17 23:51:45 +0100

    Enable ido everywhere possible and use "fuzzier" matching.

--- lisp/odabai-ido.el
+++ lisp/odabai-ido.el
@@ -22,7 +22,6 @@
 (ensure-package-installed 'ido-vertical-mode)
 ;; (ido-vertical-mode)
 
-;; -------------------------------------------------------------------------------------------------
 ;; Do not list all buffers.
 ;; -------------------------------------------------------------------------------------------------
 (defun ido-ignore-this (name)
@@ -30,11 +29,42 @@
   nil)
 (setq ido-ignore-buffers '("^ " ido-ignore-this))
 
-;; =================================================================================================
 ;; ido-imenu : It's is like imenu but with ido support
-;; =================================================================================================
+;; -------------------------------------------------------------------------------------------------
 (ensure-package-installed 'idomenu)
-
 (set-default 'imenu-auto-rescan t)
+
+;; Try to use ido everywhere possible
+;; -------------------------------------------------------------------------------------------------
+(defvar ido-enable-replace-completing-read t
+  "If t, use ido-completing-read instead of completing-read if possible.
+
+    Set it to nil using let in around-advice for functions where the
+    original completing-read is required.  For example, if a function
+    foo absolutely must use the original completing-read, define some
+    advice like this:
+
+    (defadvice foo (around original-completing-read-only activate)
+      (let (ido-enable-replace-completing-read) ad-do-it))")
+
+;; Replace completing-read wherever possible, unless directed otherwise
+(defadvice completing-read
+    (around use-ido-when-possible activate)
+  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+          (and (boundp 'ido-cur-list)
+               ido-cur-list)) ; Avoid infinite loop from ido calling this
+      ad-do-it
+    (let ((allcomp (all-completions "" collection predicate)))
+      (if allcomp
+          (setq ad-return-value
+                (ido-completing-read prompt
+                                     allcomp
+                                     nil require-match initial-input hist def))
+        ad-do-it))))
+
+;; Fuzzy matching without having to type the characters in order
+;; -------------------------------------------------------------------------------------------------
+(ensure-package-installed 'ido-better-flex)
+(ido-better-flex/enable)
 
 (provide 'odabai-ido)
