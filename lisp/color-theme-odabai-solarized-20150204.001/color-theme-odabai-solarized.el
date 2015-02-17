6b6ebf2961d8ee8ddebcc0b99bacb1ac71011cfd
Hamid Odabai <ofhamid@gmail.com>
2015-02-17 23:48:33 +0100

    Improved color theme (code+colors)

--- lisp/color-theme-odabai-solarized-20150204.001/color-theme-odabai-solarized.el
+++ lisp/color-theme-odabai-solarized-20150204.001/color-theme-odabai-solarized.el
@@ -1,91 +1,129 @@
-;; https://github.com/purcell/color-theme-sanityinc-solarized/blob/master/color-theme-sanityinc-solarized.el
-;;; TODO - backward compatible to previous Emacs versions
-;;;      - sRGB
-;;;      - colors global
-;;; Code
+;; Mixture of the following two themes:
+;; - https://github.com/purcell/color-theme-sanityinc-solarized/blob/master/color-theme-sanityinc-solarized.el
+;; - https://github.com/sellout/emacs-color-theme-solarized
+;;; TODO - backward compatible to previous Emacs versions. Should be achieved with minor-mode:
+;;;        http://stackoverflow.com/questions/23985602/emacs-creating-a-custom-highlight-parentheses-function
 
+;;; Code
 (require 'cl)
 
-(defcustom solarized-height-minus-1 0.8
-  "Font size -1."
-  :type 'number
-  :group 'solarized)
-(defcustom solarized-height-plus-1 1.1
-  "Font size +1."
-  :type 'number
-  :group 'solarized)
-(defcustom solarized-height-plus-2 1.15
-  "Font size +2."
-  :type 'number
-  :group 'solarized)
-(defcustom solarized-height-plus-3 1.2
-  "Font size +3."
-  :type 'number
-  :group 'solarized)
-(defcustom solarized-height-plus-4 1.3
-  "Font size +4."
-  :type 'number
-  :group 'solarized)
-
 ;;; Color Palette
+(defvar solarized-colors
+  ;; name    sRGB      Gen RGB   degraded  ANSI(Solarized terminal)
+  '((base03  "#002b36" "#042028" "#1c1c1c" "#7f7f7f")
+    (base02  "#073642" "#0a2832" "#262626" "#000000")
+    (base01  "#586e75" "#465a61" "#585858" "#00ff00")
+    (base00  "#657b83" "#52676f" "#626262" "#ffff00")
+    (base0   "#839496" "#708183" "#808080" "#5c5cff")
+    (base1   "#93a1a1" "#81908f" "#8a8a8a" "#00ffff")
+    (base2   "#eee8d5" "#e9e2cb" "#e4e4e4" "#e5e5e5")
+    (base3   "#fdf6e3" "#fcf4dc" "#ffffd7" "#ffffff")
+    (yellow  "#b58900" "#a57705" "#af8700" "#cdcd00")
+    (orange  "#cb4b16" "#bd3612" "#d75f00" "#ff0000")
+    (red     "#dc322f" "#c60007" "#d70000" "#cd0000")
+    (magenta "#d33682" "#c61b6e" "#af005f" "#cd00cd")
+    (violet  "#6c71c4" "#5859b7" "#5f5faf" "#ff00ff")
+    (blue    "#268bd2" "#2075c7" "#0087ff" "#0000ee")
+    (cyan    "#2aa198" "#259185" "#00afaf" "#00cdcd")
+    (green   "#859900" "#728a05" "#5f8700" "#00cd00"))
+  "This is a table of all the colors used by the Solarized color theme. Each
+   column is a different set, one of which will be chosen based on term
+   capabilities, etc.")
+
+(defcustom solarized-degrade nil
+  "For test purposes only; when in GUI mode, forces Solarized to use the 256
+degraded color mode to test the approximate color values for accuracy."
+  :type 'boolean
+  :group 'odabai-solarized)
+
+(defcustom solarized-broken-srgb (if (eq system-type 'darwin) t nil)
+  "Emacs bug #8402 results in incorrect color handling on Macs. If this is t
+(the default on Macs), Solarized works around it with alternative colors.
+However, these colors are not totally portable, so you may be able to edit
+the \"Gen RGB\" column in solarized-definitions.el to improve them further."
+  :type 'boolean
+  :group 'odabai-solarized)
+
+(defcustom solarized-termcolors 16
+  "This setting applies to emacs in terminal (non-GUI) mode.
+If set to 16, emacs will use the terminal emulator's colorscheme (best option
+as long as you've set your emulator's colors to the Solarized palette). If
+set to 256 and your terminal is capable of displaying 256 colors, emacs will
+use the 256 degraded color mode."
+  :type 'integer
+  :options '(16 256)
+  :group 'odabai-solarized)
+
 (defmacro color-theme-odabai-solarized--with-colors (mode &rest body)
-  `(let* ((base03 "#002b36")
-         (base02 "#073642")
-         (base01 "#586e75")
-         (base00 "#657b83")
-         (base0  "#839496")
-         (base1  "#93a1a1")
-         (base2  "#eee8d5")
-         (base3  "#fdf6e3")
-         (yellow "#b58900")
-         (orange "#cb4b16")
-         (red    "#dc322f")
-         (magenta"#d33682")
-         (violet "#6c71c4")
-         (blue   "#268bd2")
-         (cyan   "#2aa198")
-         (green  "#859900")
+  `(flet ((find-color (name)
+                      (let ((index (if window-system
+                                       (if solarized-degrade
+                                           3
+                                         (if solarized-broken-srgb 2 1))
+                                     (if (= solarized-termcolors 256)
+                                         3
+                                       4))))
+                        (nth index (assoc name solarized-colors)))))
+     (let* ((base03      (find-color 'base03))
+            (base02      (find-color 'base02))
+            (base01      (find-color 'base01))
+            (base00      (find-color 'base00))
+            (base0       (find-color 'base0))
+            (base1       (find-color 'base1))
+            (base2       (find-color 'base2))
+            (base3       (find-color 'base3))
 
-         (yellow-d  "#7B6000")
-         (yellow-l  "#DEB542")
-         (orange-d  "#8B2C02")
-         (orange-l  "#F2804F")
-         (red-d     "#990A1B")
-         (red-l     "#FF6E64")
-         (magenta-d "#93115C")
-         (magenta-l "#F771AC")
-         (violet-d  "#3F4D91")
-         (violet-l  "#9EA0E5")
-         (blue-d    "#00629D")
-         (blue-l    "#69B7F0")
-         (cyan-d    "#00736F")
-         (cyan-l    "#69CABF")
-         (green-d   "#546E00")
-         (green-l   "#B4C342")
+            (yellow      (find-color 'yellow))
+            (orange      (find-color 'orange))
+            (red         (find-color 'red))
+            (magenta     (find-color 'magenta))
+            (violet      (find-color 'violet))
+            (blue        (find-color 'blue))
+            (cyan        (find-color 'cyan))
+            (green       (find-color 'green))
 
-         (yellow-hc (if (eq ,mode 'light) yellow-d yellow-l))
-         (yellow-lc (if (eq ,mode 'light) yellow-l yellow-d))
-         (orange-hc (if (eq ,mode 'light) orange-d orange-l))
-         (orange-lc (if (eq ,mode 'light) orange-l orange-d))
-         (red-hc (if (eq ,mode 'light) red-d red-l))
-         (red-lc (if (eq ,mode 'light) red-l red-d))
-         (magenta-hc (if (eq ,mode 'light) magenta-d magenta-l))
-         (magenta-lc (if (eq ,mode 'light) magenta-l magenta-d))
-         (violet-hc (if (eq ,mode 'light) violet-d violet-l))
-         (violet-lc (if (eq ,mode 'light) violet-l violet-d))
-         (blue-hc (if (eq ,mode 'light) blue-d blue-l))
-         (blue-lc (if (eq ,mode 'light) blue-l blue-d))
-         (cyan-hc (if (eq ,mode 'light) cyan-d cyan-l))
-         (cyan-lc (if (eq ,mode 'light) cyan-l cyan-d))
-         (green-hc (if (eq ,mode 'light) green-d green-l))
-         (green-lc (if (eq ,mode 'light) green-l green-d)))
+            ;; colors not part of solarized theme
+            (yellow-d  "#7B6000")
+            (yellow-l  "#DEB542")
+            (orange-d  "#8B2C02")
+            (orange-l  "#F2804F")
+            (red-d     "#990A1B")
+            (red-l     "#FF6E64")
+            (magenta-d "#93115C")
+            (magenta-l "#F771AC")
+            (violet-d  "#3F4D91")
+            (violet-l  "#9EA0E5")
+            (blue-d    "#00629D")
+            (blue-l    "#69B7F0")
+            (cyan-d    "#00736F")
+            (cyan-l    "#69CABF")
+            (green-d   "#546E00")
+            (green-l   "#B4C342")
 
-     (when (eq 'light ,mode)
-       (rotatef base03 base3)
-       (rotatef base02 base2)
-       (rotatef base01 base1)
-       (rotatef base00 base0))
-     ,@body))
+            (yellow-hc (if (eq ,mode 'light) yellow-d yellow-l))
+            (yellow-lc (if (eq ,mode 'light) yellow-l yellow-d))
+            (orange-hc (if (eq ,mode 'light) orange-d orange-l))
+            (orange-lc (if (eq ,mode 'light) orange-l orange-d))
+            (red-hc (if (eq ,mode 'light) red-d red-l))
+            (red-lc (if (eq ,mode 'light) red-l red-d))
+            (magenta-hc (if (eq ,mode 'light) magenta-d magenta-l))
+            (magenta-lc (if (eq ,mode 'light) magenta-l magenta-d))
+            (violet-hc (if (eq ,mode 'light) violet-d violet-l))
+            (violet-lc (if (eq ,mode 'light) violet-l violet-d))
+            (blue-hc (if (eq ,mode 'light) blue-d blue-l))
+            (blue-lc (if (eq ,mode 'light) blue-l blue-d))
+            (cyan-hc (if (eq ,mode 'light) cyan-d cyan-l))
+            (cyan-lc (if (eq ,mode 'light) cyan-l cyan-d))
+            (green-hc (if (eq ,mode 'light) green-d green-l))
+            (green-lc (if (eq ,mode 'light) green-l green-d))
+            )
+
+       (when (eq 'light ,mode)
+         (rotatef base03 base3)
+         (rotatef base02 base2)
+         (rotatef base01 base1)
+         (rotatef base00 base0))
+       ,@body)))
 
 (defmacro color-theme-odabai-solarized--face-specs ()
   "Return a backquote which defines a list of face specs."
@@ -94,25 +132,25 @@
     (lambda (entry)
       (list (car entry) `((t ,@(cdr entry)))))
     `(
-      (default (:foreground ,base0 :background ,base03))
-      (bold (:weight bold))
-      (bold-italic (:slant italic :weight bold))
-      (underline (:underline t))
-      (italic (:slant italic))
+      (default     (:foreground ,base0 :background ,base03 ))
+      (bold        (:weight bold                           ))
+      (bold-italic (:slant italic :weight bold             ))
+      (underline   (:underline t                           ))
+      (italic      (:slant italic                          ))
 
       ;; basic
-      (cursor (:background ,red))
-      (escape-glyph-face (:foreground ,red))
-      (fringe (:foreground ,base01 :background ,base02))
-      (header-line (:foreground ,base0 :background ,base2))
-      (highlight (:background ,base02))
-      (lazy-highlight (:foreground ,cyan :background ,base03 :inverse-video t)) ;; other search results
-      (query-replcae (:foreground ,orange :background ,base03 :inverse-video t))
-      (menu (:foreground ,base0 :background ,base02))
-      (minibuffer-prompt (:foreground ,blue))
-      (mode-line (:foreground ,base1 :background ,base02 :box (:line-width 1 :color ,base1)))
-      (mode-line-buffer-id (:foreground ,base1))
-      (mode-line-inactive (:foreground ,base0  :background ,base02 :box (:line-width 1 :color ,base02)))
+      (cursor              (:background ,red                                                                    ))
+      (escape-glyph-face   (:foreground ,red                                                                    ))
+      (fringe              (:foreground ,base01 :background ,base02                                             ))
+      (header-line         (:foreground ,base0 :background ,base2                                               ))
+      (highlight           (:background ,base02                                                                 ))
+      (lazy-highlight      (:foreground ,cyan :background ,base03 :inverse-video t                              )) ;; other search results
+      (query-replcae       (:foreground ,orange :background ,base03 :inverse-video t                            ))
+      (menu                (:foreground ,base0 :background ,base02                                              ))
+      (minibuffer-prompt   (:foreground ,blue                                                                   ))
+      (mode-line           (:foreground ,base1 :background ,base02 :box (:line-width 1 :color ,base1 :style nil )))
+      (mode-line-buffer-id (:foreground ,base1                                                                  ))
+      (mode-line-inactive  (:foreground ,base0  :background ,base02 :box (:line-width 1 :color ,base02          )))
 
       ;; (region ((t (:background ,base02 :foreground ,base0 :inverse-video nil))
 
@@ -120,9 +158,9 @@
       ;; to find out if overlay is active: (equal (get-char-property (point) 'face) 'show-paren-match)
       ;; FIXME: How to remove inverse-video when smartparens is active.
       (region (:background
-                   ,(if nil
-                        base02
-                      base02) :foreground ,base0 :inverse-video nil))
+               ,(if nil
+                    base02
+                  base02) :foreground ,base0 :inverse-video nil))
 
       (secondary-selection (:background ,base02))
       (trailing-whitespace (:foreground "yellow" :inverse-video t))
@@ -186,9 +224,9 @@
       ;; org
       ;; -----------------------------------------------------------------------------------------------
       (org-agenda-structure (:foreground ,base1 :background ,base02
-                                             :weight bold :slant normal :inverse-video nil :height ,solarized-height-plus-1
-                                             :underline nil
-                                             :box (:line-width 2 :color ,base03)))
+                                         :weight bold :slant normal :inverse-video nil :height 1.1
+                                         :underline nil
+                                         :box (:line-width 2 :color ,base03)))
       (org-agenda-calendar-event (:foreground ,base1))
       (org-agenda-calendar-sexp (:foreground ,base0 :slant italic))
       (org-agenda-date (:foreground ,base01 :background ,base03 :weight normal :box (:line-width 2 :color ,base03)
@@ -226,7 +264,7 @@
       (org-table (:foreground ,green))
       (org-tag (:weight bold))
       (org-time-grid (:foreground ,base01))
-      (org-todo (:foreground ,cyan :weight bold))
+      (org-todo (:foreground ,red-hc :weight bold :box (:line-width 1 :style none)))
       (org-upcoming-deadline (:foreground ,yellow :weight normal :underline nil))
       (org-warning (:foreground ,orange :weight normal :underline nil))
       ;; org-habit
@@ -248,7 +286,7 @@
       (org-column-title (:background ,base02 :underline t :weight bold))
       (org-date-selected (:foreground ,red :inverse-video t))
       (org-document-info (:foreground ,base0))
-      (org-document-title (:foreground ,base1 :weight bold :height ,solarized-height-plus-4))
+      (org-document-title (:foreground ,base1 :weight bold :height 1.3))
       (org-drawer (:foreground ,cyan))
       (org-footnote (:foreground ,magenta :underline t))
       (org-latex-and-export-specials (:foreground ,orange))
@@ -296,9 +334,9 @@
       ;; (helm-match (:inherit match))
       (helm-selection (:background ,base02 :underline t))
       (helm-selection-line (:background ,base02 :foreground ,base1
-                                            :underline nil))
+                                        :underline nil))
       (helm-separator (:foreground ,red))
-      (helm-source-header (:background ,blue-lc :foreground ,base03 :underline nil))
+      (helm-source-header (:background ,blue :foreground ,base03 :underline nil))
       (helm-time-zone-current (:foreground ,green))
       (helm-time-zone-home (:foreground ,red))
       (helm-visible-mark (:background ,base03 :foreground ,magenta :bold t))
@@ -367,16 +405,33 @@
       ;; visual regexp
       (vr/match-0 (:inherit isearch))
 
+      ;; -----------------------------------------------------------------------------------------------
       ;; ansi-term
-      (term (:foreground nil :background nil :inherit default))
-      (term-color-black (:foreground ,base0 :background ,base03))
-      (term-color-red (:foreground ,red :background ,red))
-      (term-color-green (:foreground ,green :background ,green))
-      (term-color-yellow (:foreground ,yellow :background ,yellow))
-      (term-color-blue (:foreground ,blue :background ,blue))
-      (term-color-magenta (:foreground ,magenta :background ,magenta))
-      (term-color-cyan (:foreground ,cyan :background ,cyan))
-      (term-color-white (:foreground ,base03 :background ,base03))
+      ;; -----------------------------------------------------------------------------------------------
+      (term               (:foreground nil      :background nil :inherit default ))
+      (term-color-black   (:foreground ,base0   :background ,base03              ))
+      (term-color-red     (:foreground ,red     :background ,red                 ))
+      (term-color-green   (:foreground ,green   :background ,green               ))
+      (term-color-yellow  (:foreground ,yellow  :background ,yellow              ))
+      (term-color-blue    (:foreground ,blue    :background ,blue                ))
+      (term-color-magenta (:foreground ,magenta :background ,magenta             ))
+      (term-color-cyan    (:foreground ,cyan    :background ,cyan                ))
+      (term-color-white   (:foreground ,base03  :background ,base03              ))
+
+      ;; highlighted key words from fic-mode package
+      (font-lock-fic-face (:foreground ,red-hc :weight bold :box (:line-width 1 :style none)))
+
+      ;; -----------------------------------------------------------------------------------------------
+      ;; auto-complete
+      ;; -----------------------------------------------------------------------------------------------
+      (ac-candidate-face            (:background ,base02     :foreground ,cyan      ))
+      (ac-selection-face            (:background ,cyan-lc    :foreground ,cyan-hc   ))
+      (ac-candidate-mouse-face      (:background ,cyan-hc    :foreground ,cyan-lc   ))
+      (ac-completion-face           (:foreground ,base1      :underline t           ))
+      (ac-gtags-candidate-face      (:background ,base02     :foreground ,blue      ))
+      (ac-gtags-selection-face      (:background ,blue-lc    :foreground ,blue-hc   ))
+      (ac-yasnippet-candidate-face  (:background ,base02     :foreground ,yellow    ))
+      (ac-yasnippet-selection-face  (:background ,yellow-lc  :foreground ,yellow-hc ))
 
       ;; FIXME I do get the following error if I put (vr/match-0 (:inherit isearch)) as last line
       ;; Eager macro-expansion failure: (error "Lisp nesting exceeds `max-lisp-eval-depth'")
@@ -403,16 +458,16 @@ Argument MODE: 'light or 'dark"
 (defun color-theme-odabai-solarized (mode)
   "Apply either the dark or the light theme."
   (if (fboundp 'load-theme)
-    (let ((name (cond
+      (let ((name (cond
                    ((eq 'light mode) 'odabai-solarized-light)
                    ((eq 'dark mode) 'odabai-solarized-dark)
                    (t (error "invalid mode: %s" mode)))))
 
-      (if (boundp 'custom-enabled-themes)
-          (custom-set-variables `(custom-enabled-themes '(,name)))
-        (if (> emacs-major-version 23)
-            (load-theme name t)
-          (load-theme name))))
+        (if (boundp 'custom-enabled-themes)
+            (custom-set-variables `(custom-enabled-themes '(,name)))
+          (if (> emacs-major-version 23)
+              (load-theme name t)
+            (load-theme name))))
     (progn
       (error "Old Emacs version. Not yet handled."))))
 
