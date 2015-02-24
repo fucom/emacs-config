9c1185f5074ca8ee1a8c1dd430d21984f5265fac
Hamid Odabai <ofhamid@gmail.com>
2015-02-24 15:27:13 +0100

    Added in comment code on how to automatically open dired directories without cluttering when pressing <enter>.

--- lisp/odabai-dired.el
+++ lisp/odabai-dired.el
@@ -1,11 +1,30 @@
 ;; dired mode
+
+;;; =================================================================================================
+;;; Don't clutter with dired buffers
+;;; http://www.emacswiki.org/emacs/DiredReuseDirectoryBuffer
+;;; =================================================================================================
+;;[
 (put 'dired-find-alternate-file 'disabled nil)
 (add-hook 'dired-mode-hook
- (lambda ()
-  (define-key dired-mode-map (kbd "^")
-    (lambda () (interactive) (find-alternate-file "..")))
-  ; was dired-up-directory
-  ))
+          (lambda ()
+            (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))) ; was dired-up-directory
+            ))
+
+;; We want dired not not make always a new buffer if visiting a directory
+;; but using only one dired buffer for all directories.
+;; The same thing can be achieved by using a instead of e, f, enter
+;; (defadvice dired-find-file (around dired-subst-directory activate)
+;;   "Replace current buffer if file is a directory."
+;;   (interactive)
+;;   (message "I am activated")
+;;   (let ((orig (current-buffer))
+;;         (filename (dired-get-filename)))
+;;     ad-do-it
+;;     (when (and (file-directory-p filename)
+;;                (not (eq (current-buffer) orig)))
+;;       (kill-buffer orig))))
+;;]
 
 ;; default commands to run when you press ! on a (selection of) file(s)
 (setq dired-guess-shell-alist-user
